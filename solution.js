526566692d4a965aea16f529fad97ea1bbe3a93f
lush81_Shapovalenko <lush@i.ua>
2015-01-27 11:22:05 +0200

    Solutions to Exercises js-basics-1 from Shapovalenko

--- solution.js
+++ solution.js
@@ -1,6 +1,25 @@
 function squaresOfMax(a, b, c) {
   // Write a function that takes three numbers and returns
   // sum of squares of two largest
+  
+	if (a<=b && a<=c){
+		var max1 = b;
+		var max2 = c;
+		var maxSquare = max1*max1 + max2*max2;
+		
+		return maxSquare;
+	}else {
+		max1 = a;
+	} 
+    
+	if (b>=c){
+		var max2 = b;
+	}else {
+		var max2 = c;
+	}
+ var maxSquare = max1*max1 + max2*max2;
+  
+ return maxSquare;
 }
 
 function drawTriangle() {
@@ -13,7 +32,15 @@ function drawTriangle() {
   // #####
   // ######
   // #######
+  
+ var a="";
+  for (var i = 0; i<=6; i++){
+      a += "#";
+	  console.log (a);
+	  
+  }
 }
+
 
 function fizzBuzz() {
   // Write a program that uses console.log to print all the numbers from 1 to 100,
@@ -23,6 +50,20 @@ function fizzBuzz() {
   // When you have that working, modify your program to print "FizzBuzz",
   // for numbers that are divisible by both 3 and 5 (and still print "Fizz"
   // or "Buzz" for numbers divisible by only one of those).
+ for (var i = 1; i <= 100; i++){
+	var dividedByThree = i%3;
+	var dividedByFive = i%5;
+  
+	if(dividedByThree===0 && dividedByFive === 0){
+		console.log ("FizzBuzz");
+	}else if(dividedByThree===0 && dividedByFive!=0){
+		console.log ("Fizz");
+	}else if(dividedByThree != 0 && dividedByFive===0){
+		console.log ("Buzz");
+	}else{
+		console.log (i);
+	}
+ }
 }
 
 function chessBoard(size) {
@@ -45,7 +86,49 @@ function chessBoard(size) {
   // When you have a program that generates this pattern,
   // define a variable size = 8 and change the program so that
   // it works for any size, outputting a grid of the given width and height.
+		var grid="#";
+        var space = " ";
+        var res ="";
+        var newline = "\n";
+		
+		if (size!=0){
+
+			for(var row =1; row<=size; row++){ 
+           
+				if(row%2===0){               		 // проверяем четность строк
+
+					for (var element = 1; element <= size; element++){    // для четных строк
+                    	if(element%2===0){  			 // проверяем четность элемента
+							res=res+space;
+						}else{
+							res=res+grid;
+						}
+						
+						if(element===size && row!=size){
+					        res=res+newline;
+						}
+					}
+				}else{
+					for (var element = 1; element <= size; element++){  // для нечетных строк
+                   		if (element%2===0){
+							res=res+grid;
+						}else{
+							res=res+space;
+						}
+
+						if(element===size && row!=size){
+                        res=res+newline;}
+					}	
+				}
+
+			}
+        }		 
+	return res;
+
+   
 }
+		
+  
 
 module.exports.squaresOfMax = squaresOfMax;
 module.exports.drawTriangle = drawTriangle;
